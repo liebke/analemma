2488d690e19bc254296e6e838fcfdb14a190328d
James Elliott <james.elliott@singlewire.com>
2020-03-02 13:32:10 -0600

    Add license and url to project.clj for clojars publication.

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject analemma "1.1.0"
   :description "An SVG DSL for Clojure and ClojureScript"
+  :url "https://github.com/liebke/analemma"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]])
